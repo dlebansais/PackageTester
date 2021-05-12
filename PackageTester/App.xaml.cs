59fd5890db68d097aaece94822895d32903524f8
dlebansais <dlebansais@gmail.com>
2021-05-12 12:58:37 +0200

    Cleanup.

--- PackageTester/App.xaml.cs
+++ PackageTester/App.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace PackageTester
+﻿namespace PackageTester
 {
+    using System.Windows;
+
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// The application class.
     /// </summary>
     public partial class App : Application
     {
---
+++ README.md
@@ -0,0 +1,5 @@
+# Package Tester
+
+Test deployed packages to ensure they contain exactly what is needed and operate correctly.
+
+
